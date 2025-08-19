5d3351edf80ee41cd247d51713cca5f42c1bd409
Sushavan <sushavandas28@gmail.com>
2025-08-19 21:06:49 +0530

    Add Speech Regocnization

--- engine/command.py
+++ engine/command.py
@@ -1,13 +1,35 @@
 import pyttsx3
-
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('voice', voices[0].id)
     engine.setProperty('rate', 180)
     print(voices)
     engine.say(text)
     engine.runAndWait()
 
-speak("india is my contry, all indians are my brother and sister")
+def takecommand():
+
+    r=sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+    try:
+        print("recognizing")
+        query = r.recognize_google(audio, language = "en-in")
+        print(f"user said: {query}")
+    except Exception as e:
+        return ""
+
+    return query.lower() 
+
+
+text = takecommand()
+
+speak(text)
