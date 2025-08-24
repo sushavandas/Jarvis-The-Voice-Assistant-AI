42034979804f7a29004b039f563e194c5386215e
Sushavan <sushavandas28@gmail.com>
2025-08-24 12:53:15 +0530

    Display backend Text In Frontend. Means Backend Connect With Frontend

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import speech_recognition as sr
+import eel
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -10,26 +11,28 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+@eel.expose
 def takecommand():
 
     r=sr.Recognizer()
 
     with sr.Microphone() as source:
-        print("listening...")
+        print("listening....")
+        eel.DisplayMessage("listening....")
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
 
         audio = r.listen(source, 10, 6)
     try:
         print("recognizing")
+        eel.DisplayMessage("recognizing")
         query = r.recognize_google(audio, language = "en-in")
         print(f"user said: {query}")
+        eel.DisplayMessage(query)
+        speak(query)
+        eel.ShowHood()
     except Exception as e:
         return ""
 
     return query.lower() 
 
-
-text = takecommand()
-
-speak(text)
