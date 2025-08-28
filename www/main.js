e180af01b9e624c8f44a7f36c1ecba51b2fbecd4
Sushavan <sushavandas28@gmail.com>
2025-08-28 21:21:21 +0530

    Add Youtube and Windows App Open Feature

--- www/main.js
+++ www/main.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
         eel.playAssistantSound()
         $("#Oval").attr("hidden", true);
         $("#SiriWave").attr("hidden", false);
-        eel.takecommand()()
+        eel.allCommands()()
     });
 
 });
